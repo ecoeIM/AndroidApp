ace8fb0ee2409a404aeb9127358ffff06abe9e86
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-04-20 18:59:50 +0200

    initial

--- AndroidApp/app/src/main/java/com/example/androidapp/ui/notifications/NotificationsFragment.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/ui/notifications/NotificationsFragment.java
@@ -1,4 +1,4 @@
-package com.example.androidapp.ui.gallery;
+package com.example.androidapp.ui.notifications;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.androidapp.R;
 
-public class GalleryFragment extends Fragment {
+public class NotificationsFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
+    private NotificationsViewModel notificationsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        notificationsViewModel =
+                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        final TextView textView = root.findViewById(R.id.text_notifications);
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- AndroidApp/app/src/main/java/com/example/androidapp/ui/notifications/NotificationsViewModel.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/ui/notifications/NotificationsViewModel.java
@@ -1,16 +1,16 @@
-package com.example.androidapp.ui.gallery;
+package com.example.androidapp.ui.notifications;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class NotificationsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is notifications fragment");
     }
 
     public LiveData<String> getText() {
