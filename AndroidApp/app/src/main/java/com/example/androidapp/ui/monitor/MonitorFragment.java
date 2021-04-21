07496062e71078b86f1777cb0c732604d4c05fae
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-04-21 15:31:29 +0200

    monitor

--- AndroidApp/app/src/main/java/com/example/androidapp/ui/monitor/MonitorFragment.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/ui/monitor/MonitorFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,6 +19,10 @@ import com.example.androidapp.R;
 public class MonitorFragment extends Fragment {
 
     private MonitorViewModel monitorViewModel;
+    private ImageButton imageButtonTemperature;
+    private ImageButton imageButtonHumidity;
+    private ImageButton imageButtonCo2;
+    private ImageButton imageButtonLight;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,12 +30,21 @@ public class MonitorFragment extends Fragment {
                 new ViewModelProvider(this).get(MonitorViewModel.class);
         View root = inflater.inflate(R.layout.fragment_monitor, container, false);
         final TextView textView = root.findViewById(R.id.text_monitor);
-        monitorViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        monitorViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        imageButtonTemperature = root.findViewById(R.id.imageButtonTemperature);
+        imageButtonHumidity = root.findViewById(R.id.imageButtonHumidity);
+        imageButtonCo2 = root.findViewById(R.id.imageButtonCo2);
+        imageButtonLight = root.findViewById(R.id.imageButtonLight);
+
+        imageButtonTemperature.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+
             }
         });
+
         return root;
     }
+
 }
