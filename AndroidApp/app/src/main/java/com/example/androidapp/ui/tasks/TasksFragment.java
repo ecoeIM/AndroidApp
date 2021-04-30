f5a53043ac32a634c1c42be37b5b85d9e5b849c3
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-04-30 16:01:06 +0200

    1601

--- AndroidApp/app/src/main/java/com/example/androidapp/ui/tasks/TasksFragment.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/ui/tasks/TasksFragment.java
@@ -1,10 +1,13 @@
 package com.example.androidapp.ui.tasks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +16,12 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.androidapp.R;
+import com.example.androidapp.SettingsActivity;
 
 public class TasksFragment extends Fragment {
 
     private TasksViewModel tasksViewModel;
+    private ImageView imageViewAnime;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +30,13 @@ public class TasksFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_tasks, container, false);
         final TextView textView = root.findViewById(R.id.text_tasks);
         //tasksViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        imageViewAnime = root.findViewById(R.id.imageViewAnime);
+        imageViewAnime.setOnClickListener(v -> {
+            Toast toast = Toast.makeText(getContext(), "SHEEEEESH", Toast.LENGTH_LONG);
+            toast.show();
+        });
+
         return root;
     }
 }
