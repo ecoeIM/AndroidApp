ba22ecba1d3076174962ab05ed95be7d52bd59c5
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-04-30 19:42:16 +0200

    spinnerAdded

--- AndroidApp/app/src/main/java/com/example/androidapp/TemperatureGraphActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/TemperatureGraphActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class TemperatureGraphActivity extends AppCompatActivity {
+public class TemperatureGraphActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Toolbar toolbar;
+    private Spinner temperature_spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +26,31 @@ public class TemperatureGraphActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.temperature_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Spinner
+        temperature_spinner = findViewById(R.id.temperature_spinner);
+        temperature_spinner.setOnItemSelectedListener(this);
+        List<String> spinnerOptions = new ArrayList<>();
+        spinnerOptions.add("Last hour");
+        spinnerOptions.add("Last day");
+        spinnerOptions.add("Last 7 days");
+        spinnerOptions.add("Last 28 days");
+        spinnerOptions.add("Last 90 days");
+        spinnerOptions.add("Last 365 days");
+        spinnerOptions.add("Lifetime");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerOptions);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        temperature_spinner.setAdapter(dataAdapter);
+        //temperature_spinner.
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
