7dfce415d1ab4b23ee9c14743bdd2c573b5c1ad5
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-05-24 19:58:38 +0200

    small ui improvements

--- AndroidApp/app/src/main/java/com/example/androidapp/ui/tasks/TasksFragment.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/ui/tasks/TasksFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -14,12 +16,24 @@ import com.example.androidapp.R;
 public class TasksFragment extends Fragment {
 
     private TasksViewModel tasksViewModel;
+    private ImageButton imageButtonTasksHelp;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         tasksViewModel =
                 new ViewModelProvider(this).get(TasksViewModel.class);
         View root = inflater.inflate(R.layout.fragment_tasks, container, false);
+        imageButtonTasksHelp = root.findViewById(R.id.image_button_tasks_help);
+
+        imageButtonTasksHelp.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("This page allows you to add tasks for your terrarium. Those can be simple reminder tasks or task triggering some actions in the terrarium.");
+            builder.setPositiveButton("Close", (dialog, id) -> {
+                //close
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        });
 
         return root;
     }
