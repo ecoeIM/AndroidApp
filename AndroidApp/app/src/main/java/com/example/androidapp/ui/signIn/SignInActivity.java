aa3d57e6ec3a0da7239319109ff468ab570c4060
Eugene4JD <36538202+Eugene4JD@users.noreply.github.com>
2021-05-28 16:11:45 +0200

    ggWp

--- AndroidApp/app/src/main/java/com/example/androidapp/ui/signIn/SignInActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/ui/signIn/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidapp.ui.signIn;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.androidapp.MainActivity;
@@ -18,7 +20,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,13 +32,26 @@ import java.util.List;
 public class SignInActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 42;
     private SignInActivityViewModel viewModel;
+    private EditText editPassword;
+    private EditText editEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewModel = new ViewModelProvider(this).get(SignInActivityViewModel.class);
+        setContentView(R.layout.activity_login);
+        editPassword = findViewById(R.id.editTextTextPassword);
+        editEmail = findViewById(R.id.editTextTextEmailAddress);
+        findViewById(R.id.button_sign_in).setOnClickListener(v -> {
+            viewModel.getMauth().signInWithEmailAndPassword(editEmail.getText().toString(),editPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                    System.out.println("Aboboa");
+                }
+            });
+
+        });
         checkIfSignedIn();
-        signIn();
     }
 
     public void signIn() {
--- AndroidApp/app/src/main/java/com/example/androidapp/ui/signIn/SignInActivityViewModel.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/ui/signIn/SignInActivityViewModel.java
@@ -6,18 +6,25 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
 import com.example.androidapp.repository.UserRepository;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
 public class SignInActivityViewModel extends AndroidViewModel {
 
     private final UserRepository userRepository;
+    private FirebaseAuth mauth;
 
     public SignInActivityViewModel(Application app) {
         super(app);
         userRepository = UserRepository.getInstance(app);
+        mauth = FirebaseAuth.getInstance();
     }
 
+    public FirebaseAuth getMauth()
+    {
+        return  mauth;
+    }
     public LiveData<FirebaseUser> getCurrentUser() {
         return userRepository.getCurrentUser();
     }
