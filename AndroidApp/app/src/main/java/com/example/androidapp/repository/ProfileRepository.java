525da781e7cff319bf7069603ac006093b6c7104
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-06-02 20:31:13 +0200

    mpgraph dependency

---
+++ AndroidApp/app/src/main/java/com/example/androidapp/repository/ProfileRepository.java
@@ -0,0 +1,4 @@
+package com.example.androidapp.repository;
+
+public class ProfileRepository {
+}
--- AndroidApp/app/src/main/java/com/example/androidapp/ui/graphs/TemperatureGraphActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/ui/graphs/TemperatureGraphActivity.java
@@ -12,9 +12,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.example.androidapp.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import lecho.lib.hellocharts.model.Axis;
 import lecho.lib.hellocharts.model.Line;
@@ -26,7 +31,7 @@ import lecho.lib.hellocharts.view.LineChartView;
 public class TemperatureGraphActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Toolbar toolbar;
     private Spinner temperature_spinner;
-    private LineChartView lineChartView;
+    private LineChart lineChart;
     private TemperatureGraphActivityViewModel viewModel;
 
     @Override
@@ -56,31 +61,34 @@ public class TemperatureGraphActivity extends AppCompatActivity implements Adapt
         temperature_spinner.setAdapter(dataAdapter);
 
         //graph
-        this.lineChartView = findViewById(R.id.temperature_graph);
+        lineChart = findViewById(R.id.temperature_graph);
 
-        List<PointValue> values = new ArrayList<PointValue>();
-        values.add(new PointValue(0, 2));
-        values.add(new PointValue(1, 4));
-        values.add(new PointValue(2, 3));
-        values.add(new PointValue(3, 4));
-        values.add(new PointValue(4, 7));
-
-        //In most cased you can call data model methods in builder-pattern-like manner.
-        Line line = new Line(values).setColor(Color.parseColor("#008e54")).setCubic(true);
-        List<Line> lines = new ArrayList<>();
-        lines.add(line);
-
-        LineChartData data = new LineChartData();
-        data.setLines(lines);
-        Axis axisX = new Axis();
-        axisX.setName("Time");
-        Axis axisY = new Axis();
-        axisY.setName("Temperature");
-
-        data.setAxisXBottom(axisX);
-        data.setAxisYLeft(axisY);
-
-        lineChartView.setLineChartData(data);
+//        //graph
+//        this.lineChartView = findViewById(R.id.temperature_graph);
+//
+//        List<PointValue> values = new ArrayList<PointValue>();
+//        values.add(new PointValue(0, 2));
+//        values.add(new PointValue(1, 4));
+//        values.add(new PointValue(2, 3));
+//        values.add(new PointValue(3, 4));
+//        values.add(new PointValue(4, 7));
+//
+//        //In most cased you can call data model methods in builder-pattern-like manner.
+//        Line line = new Line(values).setColor(Color.parseColor("#008e54")).setCubic(true);
+//        List<Line> lines = new ArrayList<>();
+//        lines.add(line);
+//
+//        LineChartData data = new LineChartData();
+//        data.setLines(lines);
+//        Axis axisX = new Axis();
+//        axisX.setName("Time");
+//        Axis axisY = new Axis();
+//        axisY.setName("Temperature");
+//
+//        data.setAxisXBottom(axisX);
+//        data.setAxisYLeft(axisY);
+//
+//        lineChartView.setLineChartData(data);
     }
 
     @Override
@@ -113,5 +121,25 @@ public class TemperatureGraphActivity extends AppCompatActivity implements Adapt
         onBackPressed();
         finish();
         return true;
+    }
+
+    public void setLineChartData() {
+        List<String> xValues = new ArrayList<>();
+        xValues.add("11.00 AM");
+        xValues.add("12.00 AM");
+        xValues.add("1.00 PM");
+        xValues.add("2.00 PM");
+        xValues.add("3.00 PM");
+
+        List<Entry> listEntries = new ArrayList<>();
+        listEntries.add(new Entry(20f, 0));
+        listEntries.add(new Entry(30f, 1));
+        listEntries.add(new Entry(40f, 2));
+        listEntries.add(new Entry(50f, 3));
+        listEntries.add(new Entry(60f, 5));
+
+        LineDataSet lineDataSet = new LineDataSet(listEntries, "Test");
+
+        LineData data = new LineData(lineDataSet);
     }
 }
